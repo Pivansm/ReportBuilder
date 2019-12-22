8995c37caffd6d02076092d967ddbb50acbfd305
Pivansm <pivansm@outlook.com>
2019-12-22 19:36:59 +0300

    Java diplom updt 1

---
+++ src/main/java/skeleton/TreeViewDemo.java
@@ -0,0 +1,32 @@
+package skeleton;
+
+
+import javax.swing.*;
+
+class TreeViewTable extends JFrame {
+
+    private JTree tree;
+    private JTable table;
+
+    public TreeViewTable() {
+
+        tree = new JTree();
+        table = new JTable();
+
+        JSplitPane splitPane = new JSplitPane(
+                JSplitPane.HORIZONTAL_SPLIT, true,
+                new JScrollPane(tree),
+                new JScrollPane(table)
+        );
+
+        getContentPane().add(splitPane);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TreeViewTable();
+    }
+}
