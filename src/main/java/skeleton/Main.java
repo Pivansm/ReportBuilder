526451397e5aa4f71b62325809278e3072cc7293
Pivansm <pivansm@outlook.com>
2020-01-12 21:27:19 +0300

    RB diplom update 8

--- src/main/java/skeleton/Main.java
+++ src/main/java/skeleton/Main.java
@@ -1,9 +1,6 @@
 package skeleton;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 import java.sql.SQLException;
 
 public class Main {
